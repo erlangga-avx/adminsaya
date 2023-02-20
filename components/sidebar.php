fe87707c1f480c7b6a5a1ff373b2250223109e55
erlangga-avx <anggatausendiri@gmail.com>
2023-02-20 13:25:15 +0800

    mengubah icon sidebar

--- components/sidebar.php
+++ components/sidebar.php
@@ -3,23 +3,23 @@
                     <div class="sb-sidenav-menu">
                         <div class="nav">
                             <a class="nav-link" href="index.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-box-open"></i></div>
                                 Stok Barang
                             </a>
                             <a class="nav-link" href="masuk.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-download"></i></div>
                                 Barang Masuk
                             </a>
                             <a class="nav-link" href="keluar.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-upload"></i></div>
                                 Barang Keluar
                             </a>
                             <a class="nav-link" href="supplier.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-headset"></i></div>
                                 Supplier
                             </a>
                             <a class="nav-link" href="alat.php">
-                                <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                                <div class="sb-nav-link-icon"><i class="fas fa-tools"></i></div>
                                 Status Alat
                             </a>
                             <a class="nav-link" href="logout.php">
