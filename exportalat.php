d154687610c442e1e57821f9af322bf20d5f6536
erlangga-avx <anggatausendiri@gmail.com>
2023-02-21 13:49:27 +0800

    mengganti judul export

--- exportalat.php
+++ exportalat.php
@@ -4,7 +4,7 @@ require 'cek.php';
 ?>
 <html>
 <head>
-  <title>Stock Barang</title>
+  <title>GRAND Fotocopy Gambut</title>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
   <link rel="stylesheet" href="https://cdn.datatables.net/1.13.2/css/dataTables.bootstrap5.min.css">
   <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
