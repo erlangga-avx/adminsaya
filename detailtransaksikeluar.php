828ac68532bdd8cb8eb31286ed2fc2a608d15a46
erlangga-avx <anggatausendiri@gmail.com>
2023-08-05 07:57:52 +0800

    detail transaksi masuk & keluar

--- detailtransaksikeluar.php
+++ detailtransaksikeluar.php
@@ -5,13 +5,11 @@ require 'cek.php';
 //mendapatkan id barang yang dioper dari halaman sebelumnya
 $idtransaksi = $_GET['id']; //get id transaksi
 //mengambil informasi barang berdasarkan database
-$get = mysqli_query($conn, "select * from transaksimasuk where idtransaksi='$idtransaksi'");
+$get = mysqli_query($conn, "select * from transaksikeluar where idtransaksi='$idtransaksi'");
 $fetch = mysqli_fetch_assoc($get);
 //set variabel
 $tanggal = $fetch['tanggal'];
 $kodetransaksi = $fetch['kodetransaksi'];
-$pengirim = $fetch['pengirim'];
-$penerima = $fetch['penerima'];
 $jumlah = $fetch['jumlah'];
 ?>
 
@@ -25,7 +23,7 @@ $jumlah = $fetch['jumlah'];
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <meta name="description" content="" />
     <meta name="author" content="" />
-    <title>Detail Transaksi Masuk - GRAND Inventory</title>
+    <title>Detail Transaksi Keluar - GRAND Inventory</title>
     <link href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css" rel="stylesheet" />
     <link href="css/styles.css" rel="stylesheet" />
     <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
@@ -40,22 +38,18 @@ $jumlah = $fetch['jumlah'];
         <div id="layoutSidenav_content">
             <main>
                 <div class="container-fluid px-4">
-                    <h1 class="mt-4">Detail Transaksi Masuk</h1>
-                    <a href="exportdetailtransaksimasuk.php?id=<?= $idtransaksi; ?>" class="btn btn-success">Export Data</a>
+                    <h1 class="mt-4">Detail Transaksi Keluar</h1>
+                    <a href="exportdetailtransaksikeluar.php?id=<?= $idtransaksi; ?>" class="btn btn-success">Export Data</a>
                     <div class="card mb-4 mt-4">
                         <div class="card-header">
                             <h2>Kode Transaksi <?= $kodetransaksi; ?></h2>
                         </div>
                         <div class="card-body">
-                            <input type="text" name="tanggal" value="Tanggal Masuk : <?php echo $tanggal; ?>" class="form-control" disabled>
-                            <br>
-                            <input type="text" name="pengirim" value="Pengirim : <?php echo $pengirim; ?>" class="form-control" disabled>
-                            <br>
-                            <input type="text" name="penerima" value="Penerima : <?php echo $penerima; ?>" class="form-control" disabled>
+                            <input type="text" name="tanggal" value="Tanggal Keluar : <?php echo $tanggal; ?>" class="form-control" disabled>
 
                             <br></br>
 
-                            <h3>Data Barang Masuk</h3>
+                            <h3>Data Barang Keluar</h3>
                             <div class="card mb-4">
                                 <div class="table-responsive">
                                     <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
@@ -64,38 +58,33 @@ $jumlah = $fetch['jumlah'];
                                                 <th>Nama Barang</th>
                                                 <th>Jumlah</th>
                                                 <th>Satuan</th>
-                                                <th>Supplier</th>
-                                                <th>Nomor Nota</th>
+                                                <th>Keterangan</th>
                                             </tr>
                                         </thead>
                                         <tbody>
                                             <?php
 
-                                            $query = "select * from masuk m join stok s on s.idbarang = m.idbarang join supplier su on su.idsupplier = m.idsupplier where m.idtransaksi='$idtransaksi'";
+                                            $query = "select * from keluar k join stok s on s.idbarang = k.idbarang where k.idtransaksi='$idtransaksi'";
 
-                                            $ambildatamasuk = mysqli_query($conn, $query);
+                                            $ambildatakeluar = mysqli_query($conn, $query);
                                             $i = 1;
 
-                                            while ($data = mysqli_fetch_array($ambildatamasuk)) {
+                                            while ($data = mysqli_fetch_array($ambildatakeluar)) {
                                                 $idb = $data['idbarang'];
-                                                $idm = $data['idmasuk'];
+                                                $idk = $data['idkeluar'];
                                                 $idt = $data['idtransaksi'];
                                                 $tanggal = $data['tanggal'];
                                                 $satuan = $data['satuan'];
                                                 $namabarang = $data['namabarang'];
                                                 $qty = $data['qty'];
-                                                $ids = $data['idsupplier'];
-                                                $namasupplier = $data['namasupplier'];
-                                                $nota = $data['nota'];
-
+                                                $keterangan = $data['keterangan'];
                                             ?>
 
                                                 <tr>
                                                     <td><?= $namabarang; ?></td>
                                                     <td><?= $qty; ?></td>
                                                     <td><?= $satuan; ?></td>
-                                                    <td><?= $namasupplier; ?></td>
-                                                    <td><?= $nota; ?></td>
+                                                    <td><?= $keterangan; ?></td>
                                                 </tr>
                                 </div>
                             </div>
